d0e2f85762b0f421316a5aa2f5bbeba5eaa0fdbb
dheerajkrishna458 <dheerajkittu27@gmail.com>
2022-10-26 17:43:36 -0500

    last

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # f2wa09Mekala
-remote render server [render server](https://f2wa09mekala.onrender.com/)
+remote render server https://f2wa09mekala.onrender.com/    
+remote render website server link https://dashboard.render.com/web/srv-cdaq73irrk09hipnm2l0/deploys/dep-cdbhbrha6gdlcss2komg
--- routes/computation.js
+++ routes/computation.js
@@ -3,9 +3,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-var x= Math.floor(Math.random())
+var x= Math.random()
 router.get('/', function(req, res, next) {
-  res.render('computation', { title: 'computation on server' },{result: 'Math.abs() applied to '+x+' is '+Math.abs(x)},{result1: 'Math.acos() applied to '+x+' is '+Math.acos(x)},{result2: 'Math.sin() applied to '+x+' is '+Math.sin(x)}),{result: 'Math.sinh() applied to '+x+' is '+Math.sinh(x)};
+  res.render('computation', { 
+    title: 'computation on server' , 
+    result: 'Math.abs() applied to '+x+' is '+Math.abs(x),
+    result1: 'Math.acos() applied to '+x+' is '+Math.acos(x), 
+    result2: 'Math.sin() applied to '+x+' is '+Math.sin(x), 
+    result3: 'Math.sinh() applied to '+x+' is '+Math.sinh(x)
+  })
 });
 
 module.exports = router;
